0ce747abcbd477391584727f2e5bcccd5f84010d
Alan Bednarski <78023436+Alanb2001@users.noreply.github.com>
2021-11-23 10:55:10 +0000

    SphereToSphere Chnages

--- SphereToSphere.c
+++ SphereToSphere.c
@@ -1,15 +1,5 @@
-/*******************************************************************************************
-*
-*   raylib [models] example - Detect basic 3d collisions (box vs sphere vs box)
-*
-*   This example has been created using raylib 1.3 (www.raylib.com)
-*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
-*
-*   Copyright (c) 2015 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
-
 #include "raylib.h"
+#include "raymath.h"
 
 typedef struct {
     Vector3 position;
@@ -21,39 +11,64 @@ typedef struct {
 
 int main(void)
 {
-    // Initialization
-    //--------------------------------------------------------------------------------------
     const int numSpheres = 2;
     const int screenWidth = 800;
     const int screenHeight = 450;
 
-    InitWindow(screenWidth, screenHeight, "raylib [models] example - box collisions");
+    InitWindow(screenWidth, screenHeight, "SphereToSphere");
 
-    // Define the camera to look into our 3d world
     Camera camera = { { 0.0f, 10.0f, 10.0f }, { 0.0f, 0.0f, 0.0f }, { 0.0f, 1.0f, 0.0f }, 45.0f, 0 };
     
     Sphere spheres[numSpheres];
     spheres[0].position = (Vector3){0.0f, 1.0f, 2.0f};
     spheres[0].radius = (float){1.5f};
     spheres[0].sphereColour = GREEN;
+    spheres[0].velocity = (Vector3){1.0f, 0.0f, 0.0f};
+    spheres[0].mass = (float){1.0f};
     
     spheres[1].position = (Vector3){0.0f, 1.0f, 5.0f};
     spheres[1].radius = (float){1.5f};
     spheres[1].sphereColour = RED;
+    spheres[1].velocity = (Vector3){-1.0f, 0.0f, 0.0f};
+    spheres[1].mass = (float){1.0f};
 
+    Vector3 p1 = spheres[0].position;
+    Vector3 p2 = spheres[1].position;
+    float r1 = spheres[0].radius;
+    float r2 = spheres[1].radius;
+    Vector3 v1 = spheres[0].velocity;
+    Vector3 v2 = spheres[1].velocity;
+    float a = (Vector3LengthSqr(Vector3Scale(Vector3Subtract(v1,v2),GetFrameTime())));
+    float b = Vector3CrossProduct()
     
 
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
+    //Unity code
+    //Vector3 p1 = spheres[j].transform.position;
+    //Vector3 p2 = spheres[i].transform.position;
+    //float r1 = spheres[j].GetComponent<MeshRenderer>().bounds.extents.magnitude;
+    //float r2 = spheres[i].GetComponent<MeshRenderer>().bounds.extents.magnitude;
+    //Vector3 v1 = spheres[j].GetComponent<StartingVelocity>().velocity;
+    //Vector3 v2 = spheres[i].GetComponent<StartingVelocity>().velocity;
+    //float a = ((v1 - v2) * Time.deltaTime).sqrMagnitude;
+    //float b = Vector3.Dot(p1 - p2, (v1 - v2) * Time.deltaTime) * 2;
+    //float c = (p1 - p2).sqrMagnitude - (r1 + r2) * (r1 + r2);
+    //float t = (-b - Mathf.Sqrt(b * b - 4 * a * c)) / (2 * a);
+    //
+    //if (t < 1 && t > 0)
+    //{
+    //    print(t);
+    //    print("Hit");
+    //    return true;
+    //}
+
+    while (!WindowShouldClose())
     {
-        // Update here
         BeginDrawing();
 
             ClearBackground(RAYWHITE);
                 
-            BeginMode3D(camera);
+            BeginMode3D(camera);          
             
-            // Draw here
             for(int i = 0; i < numSpheres; i++)
             {
                DrawSphere(spheres[i].position, spheres[i].radius, spheres[i].sphereColour);
@@ -67,11 +82,8 @@ int main(void)
 
         EndDrawing();
     }
-
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
+    
+    CloseWindow();
 
     return 0;
 }
